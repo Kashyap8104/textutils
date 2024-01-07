5d61a4f2244d8bd6db37b423380699398d59d9e8
Kashyap Manvar <kashyapmanvar18@gmail.com>
2024-01-07 16:19:39 +0530

    Updates

---
+++ precache-manifest.778dae34031004f5614b49a960f0a30a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c816a2763f47859bf54a4abbb69edd23",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "c3e248de8ac4cd648ca4",
+    "url": "/textutils/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "7232ee8b8b837bd08334",
+    "url": "/textutils/static/js/2.4cf87792.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils/static/js/2.4cf87792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d41e85a5c12099614ca",
+    "url": "/textutils/static/js/3.c6f67c18.chunk.js"
+  },
+  {
+    "revision": "c3e248de8ac4cd648ca4",
+    "url": "/textutils/static/js/main.e52d56bc.chunk.js"
+  },
+  {
+    "revision": "daba6dc0575d64378ab4",
+    "url": "/textutils/static/js/runtime-main.913c4c48.js"
+  }
+]);
